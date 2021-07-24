e04582c688d11fb55a8df3cae05439e3380c9890
Robert <services@roberthass.de>
2021-07-24 11:07:32 +0200

    dummy endpoints

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,48 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/courses', function () {
+    return json_encode(array("courses" => array(array("id" => 1, "name" => "Test Kurs 1"), array("id" => 2, "name" => "Test Kurs 2"))));
+});
+
+$router->get('/students', function () {
+    $now = new \DateTime();
+    $students = array(
+        array(
+            "id" => 1,
+            "firstNane" => "Gina",
+            "givenName" => "Duncan",
+            "photoUrl" => "https://randomuser.me/api/portraits/women/65.jpg",
+            "lastPraise" => $now->format("Y-m-d H:i"),
+            "praiseCount" => 24
+        ),
+        array(
+            "id" => 2,
+            "firstNane" => "Jessie",
+            "givenName" => "Robertson",
+            "photoUrl" => "https://randomuser.me/api/portraits/men/79.jpg",
+            "lastPraise" => $now->format("Y-m-d H:i"),
+            "praiseCount" => 18
+        )
+    );
+    return json_encode(array("students" => $students));
+});
+
+$router->post('/praise', function () {
+    $now = new \DateTime();
+
+    $student =  array(
+        "id" => 2,
+        "firstNane" => "Jessie",
+        "givenName" => "Robertson",
+        "photoUrl" => "https://randomuser.me/api/portraits/men/79.jpg",
+        "lastPraise" => $now->format("Y-m-d H:i"),
+        "praiseCount" => 18
+    );
+    return json_encode($student);
+});
+
+$router->get('/translations', function () {
+    return json_encode(array("translations" => array(array("id" => 1, "text" => "text 1", "score" => 0.98))));
+});
