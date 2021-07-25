ea9c28dfb8fef7ccba06e38d7bdeda62fb112288
CmdrXay <github.com@null-domain.de>
2021-07-25 13:23:13 +0200

    cors issue with options

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -7,8 +7,12 @@ class CorsMiddleware {
     $response = $next($request);
 
     $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+    // $response->header('Access-Control-Allow-Headers', '*');
     $response->header('Access-Control-Allow-Origin', '*');
+    $response->header('Access-Control-Request-Method', 'POST');
+    $response->header('Access-Control-Allow-Credentials', 'true');
+    $response->header('Access-Control-Max-Age', '86400');
+    $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
     return $response;
   }
